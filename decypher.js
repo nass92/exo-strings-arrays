dc812ff420ca4a88c9863f0ec8eb7804ab679d7d
nass92 <goujanassim@hotmail.com>
2021-02-19 09:24:26 +0100

    ewe

--- decypher.js
+++ decypher.js
@@ -1,7 +1,8 @@
 
 function code(str) {
 
-  let solved = "";
+  let sol
+    = "";
   let X = 17
 
   for (let i = 0; i < str.length; i++) {
@@ -10,16 +11,21 @@ function code(str) {
 
     // 65 dans le tableau ascii = A; et 77= N; 90 = Z
     if (asciiNum >= 65 && asciiNum <= 65) {
-      solved += String.fromCharCode(asciiNum + X);
+      sol
+        += String.fromCharCode(asciiNum + X);
     } else if (asciiNum >= 82 && asciiNum <= 90) {
-      solved += String.fromCharCode(asciiNum - X);
+      sol
+        += String.fromCharCode(asciiNum - X);
     } else if (asciiNum >= 66 && asciiNum <= 81) {
-      solved += String.fromCharCode(asciiNum + 9);
+      sol
+        += String.fromCharCode(asciiNum + 9);
     } else {
-      solved += str[i];
+      sol
+        += str[i];
     }
   }
-  return solved;
+  return sol
+    ;
 }
 console.log(code("YRMV PFL VMVI YRU R UIVRD EVF KYRK PFL NVIV JF JLIV NRJ IVRC NYRK ZW PFL NVIV LERSCV KF NRBV WIFD KYRK UIVRD YFN NFLCU PFL BEFN KYV UZWWVIVETV SVKNVVE KYV UIVRD NFICU REU KYV IVRC NFICU"))
 
